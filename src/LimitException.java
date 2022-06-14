9f48bfc5f8f711e667b95146636bcd73c34d9133
AsanTU <rebet(24>
2022-06-14 17:17:28 +0600

    Initial commit

---
+++ src/LimitException.java
@@ -0,0 +1,17 @@
+public class LimitException extends Exception{
+
+    public double getRemainingAmmount() {
+        return remainingAmmount;
+    }
+
+    public void setRemainingAmmount(double remainingAmmount) {
+        this.remainingAmmount = remainingAmmount;
+    }
+
+    public LimitException(String message, double remainingAmmount) {
+        super(message);
+        this.remainingAmmount = remainingAmmount;
+    }
+
+    private double remainingAmmount;
+}
